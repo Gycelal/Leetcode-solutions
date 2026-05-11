3e1c5386e5d361aad9fd4a39865383af91600a5a
Gycelal <krgycelal@gmail.com>
2026-05-11 10:41:23 +0530

    solution: 567 Permutation in string

---
+++ Sliding Window/567_Permutation_in_string.py
@@ -0,0 +1,59 @@
+# Not an optimized solution
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+
+        left, right = 0, len(s1)
+
+        while right <= len(s2):
+            sub = s2[left:right]
+            if sorted(s1) == sorted(sub):
+                return True
+            else:
+                left += 1
+                right += 1        
+        return False
+    
+
+
+# O(n) time and O(1) space complexity solution
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+
+        if len(s1) > len(s2): return False
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26: return True
+
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+            
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+            l += 1
+        return matches == 26
